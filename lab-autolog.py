414eeb6fa188039d399b93089edb707808b5d56e
Karolis Misiunas <k.misiunas@gmail.com>
2015-10-23 12:07:16 +0100

    function structure

--- lab-autolog.py
+++ lab-autolog.py
@@ -21,6 +21,24 @@ accelerometerUpdateInterval = 0.01 # in sec
 ### CODE ###
 
 sense = SenseHat()
+
+def sendInforToServer( sense, vibration ):
+   "function_docstring"
+   function_suite
+   return [expression]
+   
+def vibrationDetector( sense, vibration ):
+   "function_docstring"
+   function_suite
+   return [expression]   
+
+
+# Init
+
+## Clear the screen
+
+
+vibration = [0.0, 0.0, 0.0]
 timeOld = time.time()
 timeOldAccelerometer = time.time()
 
@@ -28,16 +46,21 @@ while(True):
   # check if time elapsed 
   if(time.time() >= timeOld):
     # Send infor to the server
-    sendInforToServer()
+    sendInforToServer(sense, vibration)
     timeOld += updateInterval
     
   # Update aceleroter
   if(time.time() >= timeOldAccelerometer):
-    vibrationDetector()
+    vibrationDetector(sense, vibration)
     timeOld += acceleroterUpdateInterval
     if( time.time() - timeOldAccelerometer > 10*  accelerometerUpdateInterval):
       print( 'Falling behind accelerometers schedule at ' + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) )
   
+  # blink LED somewhere
+  
 
 
-# Log failure!
+# Log failure
+print( 'lab-autolog shutdown at ' + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) )
+
+# print red X on the hat to let the user know that t needs restatting
