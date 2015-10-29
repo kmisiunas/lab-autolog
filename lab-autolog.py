c795599cd18d5908a94cebdea0ae75ffdcca061c
Karolis Misiunas <k.misiunas@gmail.com>
2015-10-29 15:05:39 +0000

    mean measurements

--- lab-autolog.py
+++ lab-autolog.py
@@ -28,16 +28,14 @@ accelerometerUpdateInterval = 0.1 # in sec
 
 ### CODE ###
 
-sense = SenseHat()
-
 def sendInforToServer( sense, vibration ):
    "reads infor and sends it to the server"
    print("debug: send data request at "+ time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) )
    url = serverURL + publicKey + "?private_key=" + privateKey
-   #url = wolframServer + debianID
-   vals = "&temp=%.2f" % tempCalibrated(sense)
-   vals += "&humidity=%.3f" % sense.get_humidity()
-   vals += "&pressure=%.3f" % sense.get_pressure()
+   #url = wolframServer + debianID # wolfram DataDrop
+   vals = "&temp=%.2f" % sum(temp)/len(temp)
+   vals += "&humidity=%.3f" % sum(humidity)/len(humidity)
+   vals += "&pressure=%.3f" % sum(pressure)/len(pressure)
    vals += "&cpu_temp=%.2f" % cpuTemp()
    vals += "&light=%.3f" % 0.0
    vals += "&vibration=%.3f" % 0.0
@@ -48,10 +46,11 @@ def sendInforToServer( sense, vibration ):
       #s = f.read()
       f.close()
    except Exception as e:
-     print("There was an error: %r" % e)   
+     print("There was an error: %r" % e)  
+   clearAccumulate()
    return []
    
-def tempCalibrated( sense ):
+def tempCalibrated():
    return (sense.get_temperature() + tempOffset) * tempScale
    
 def readAndResetVibration( vibration ):
@@ -66,14 +65,34 @@ def cpuTemp():
    tmp = float( os.popen('cat /sys/class/thermal/thermal_zone0/temp').readline() )
    return (tmp/1000)
    
+def measureAccumulate():
+   "Function for accumulating measurements"
+   global temp, humidity, pressure
+   temp.append( tempCalibrated() )
+   humidity.append( sense.get_humidity() )
+   pressure.append( sense.get_pressure() )
+   
+def clearAccumulate():
+   "Function for clearing accumulated data forn new averaging"
+   global temp, humidity, pressure
+   temp = []
+   pressure = []
+   humidity = []
 
 
 # Init
+
+sense = SenseHat()
 
 ## Clear the screen
 #sense.show_message("")
 sense.clear(0, 0, 0)
 sense.low_light = True
+
+# Prepare arrays 
+temp = [tempCalibrated()]
+pressure = [sense.get_pressure()]
+humidity = [sense.get_humidity()]
 
 # vibration: [ sum of |x|, sum of x^2, n] 
 vibration = [0.0, 0.0, 0.0]
@@ -86,7 +105,8 @@ while(True):
     # Send infor to the server
     sendInforToServer(sense, vibration)
     timeOld += updateInterval
-    
+
+  
   # Update aceleroter
   #if(time.time() >= timeOldAccelerometer):
    # vibrationDetector(sense, vibration)
